a48a8105d8d1892c2dbbe4f09ecaff868d4af8ee
Daniël de Kok <me@danieldk.eu>
2011-09-02 22:26:52 +0200

    Adler32: use lazy ByteStrings in the implementation.

--- src/Data/Digest/Pure/Adler32.hs
+++ src/Data/Digest/Pure/Adler32.hs
@@ -15,7 +15,8 @@ module Data.Digest.Pure.Adler32 (
 ) where
 
 import Data.Bits ((.&.), (.|.), shiftL, shiftR)
-import qualified Data.ByteString as B
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
 import Data.Word (Word8, Word32)
 
 -- |
@@ -32,27 +33,31 @@ class Adler32 a where
 
 data AdlerState = AdlerState {-# UNPACK #-} !Word32  {-# UNPACK #-} !Word32
 
-instance Adler32 B.ByteString where
+instance Adler32 BL.ByteString where
   adler32       = adler32Update adler32Initial
   adler32Update = adler32Update'
+
+instance Adler32 BS.ByteString where
+  adler32             = adler32Update adler32Initial
+  adler32Update state = adler32Update state . BL.fromChunks . return
 
 adler32Initial :: Word32
 adler32Initial = 1
 
-adler32Update' :: Word32 -> B.ByteString -> Word32
+adler32Update' :: Word32 -> BL.ByteString -> Word32
 adler32Update' adlerState =
   combineAdler . loop (breakAdler adlerState)
   where
     nmax = 5552
     loop adlerState' str
-      | B.null xs = adlerState'
+      | BL.null xs = adlerState'
       | otherwise = loop (adler32Chunk adlerState' xs) ys
         where
-          (xs, ys) = B.splitAt nmax str
+          (xs, ys) = BL.splitAt nmax str
 
-adler32Chunk :: AdlerState -> B.ByteString -> AdlerState
+adler32Chunk :: AdlerState -> BL.ByteString -> AdlerState
 adler32Chunk adler =
-  modAdler . B.foldl' sumAdler adler
+  modAdler . BL.foldl' sumAdler adler
 
 breakAdler :: Word32 -> AdlerState
 breakAdler adler =
