1b7752f32a310c2f6aba9629f831eeaf7481736b
Daniël de Kok <me@danieldk.eu>
2011-09-04 18:08:02 +0200

    Test CRC32 against the zlib implementation.

--- test/TestSuiteDigestPure.hs
+++ test/TestSuiteDigestPure.hs
@@ -4,7 +4,9 @@ module Main where
 
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Digest.Adler32 as A
+import qualified Data.Digest.CRC32 as C
 import qualified Data.Digest.Pure.Adler32 as AP
+import qualified Data.Digest.Pure.CRC32 as CP
 import System.Exit
 import Test.QuickCheck
 import Text.Printf
@@ -26,8 +28,14 @@ succesful :: Result -> Bool
 succesful (Success _ _ _) = True
 succesful _               = False
 
-tests = [("aldler32CompareZlib",  quickCheckResult adler32CompareZlib)]
+tests = [
+  ("adler32CompareZlib", quickCheckResult adler32CompareZlib),
+  ("crc32CompareZlib",   quickCheckResult crc32CompareZlib) ]
 
 adler32CompareZlib :: B.ByteString -> Bool
 adler32CompareZlib str =
   AP.adler32 str == A.adler32 str
+
+crc32CompareZlib :: B.ByteString -> Bool
+crc32CompareZlib str =
+  CP.crc32 str == C.crc32 str
